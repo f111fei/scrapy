0072bef7ee722dd14ab024e153a8ef1a68d7d7e2
Krisan Alfa Timur <krisan47@gmail.com>
2017-07-07 15:16:16 +0700

    [UPDATE] Lint everything.

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
-import Crawler from 'crawler';
-import restify from 'restify';
-const server = restify.createServer();
-const scrape = function (url, callback) {
-  new Crawler({
-    callback: function (error, res, done) {
-      if (error) {
-        callback(error, null);
-      } else {
-        callback(null, res.$);
-      }
-
-      done();
-    }
-  }).queue(url);
-}
+var restify = require('restify')
+var scraper = require('./lib/scraper')
+const server = restify.createServer()
 
 // Configuration
 server.name = 'Scrapy'
 
 // Routing
 server.get('/', function (req, res) {
-  const url = 'https://www.tripadvisor.com/Hotels-g297701-Ubud_Bali-Hotels.html';
+  const url = 'https://www.tripadvisor.com/Hotels-g297701-Ubud_Bali-Hotels.html'
 
-  scrape(url, function (err, $) {
+  scraper(url, function (err, $) {
     if (err) {
-      res.status = 500;
+      res.status = 500
 
       res.send({
         message: 'Something bad happened.',
         error: err
-      });
+      })
     } else {
       res.send({
         message: 'Crawled successfully.',
@@ -39,12 +26,12 @@ server.get('/', function (req, res) {
           description: $('meta[property="og:description"]').attr('content'),
           image: $('meta[property="og:image"]').attr('content')
         }
-      });
+      })
     }
-  });
-});
+  })
+})
 
 // Run the server once everything is ready
 server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
+  console.log('%s listening at %s', server.name, server.url)
+})
